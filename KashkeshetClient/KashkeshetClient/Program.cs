5081273794cb8bf113312b11d26409550fbdd486
thelittlecitizen12 <thelittlecitizen12@gmail.com>
2020-09-07 14:06:20 +0300

    Created KashkeshetClient project

---
+++ KashkeshetClient/KashkeshetClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KashkeshetClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
