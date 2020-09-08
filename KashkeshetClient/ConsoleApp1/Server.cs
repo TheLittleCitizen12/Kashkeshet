a1b8e4061020020c31e3b90a66406c0ae04558d5
thelittlecitizen12 <thelittlecitizen12@gmail.com>
2020-09-08 09:11:11 +0300

    Added broadcast chat in main of Server and Client

---
+++ KashkeshetClient/ConsoleApp1/Server.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KashkeshetServer
+{
+    class Server
+    {
+        
+
+        public void StrartServer()
+        {
+
+
+            
+
+        }
+        
+    }
+}
--- KashkeshetClient/KashkeshetClient/Program.cs
+++ KashkeshetClient/KashkeshetClient/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
 namespace KashkeshetClient
 {
@@ -6,7 +10,41 @@ namespace KashkeshetClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IPAddress ip = IPAddress.Parse("10.1.0.20");
+            int port = 11000;
+            TcpClient client = new TcpClient();
+            client.Connect(ip, port);
+            Console.WriteLine("client connected!!");
+            NetworkStream ns = client.GetStream();
+            Thread thread = new Thread(o => ReceiveData((TcpClient)o));
+
+            thread.Start(client);
+
+            string s;
+            while (!string.IsNullOrEmpty((s = Console.ReadLine())))
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(s);
+                ns.Write(buffer, 0, buffer.Length);
+            }
+
+            client.Client.Shutdown(SocketShutdown.Send);
+            thread.Join();
+            ns.Close();
+            client.Close();
+            Console.WriteLine("disconnect from server!!");
+            Console.ReadKey();
+        }
+        static void ReceiveData(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+            byte[] receivedBytes = new byte[1024];
+            int byte_count;
+
+            while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+            {
+                Console.Write(Encoding.ASCII.GetString(receivedBytes, 0, byte_count));
+            }
         }
     }
+    
 }
